348e4945902a71508afb45cbd516c3d09fa62eb7
Sigfrido G.P. <sigfrido.gonzalezpuga@gmail.com>
2026-08-18 08:09:37 -0400

    feat: ajouter la connexion PDO

---
+++ src/Database.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+final class Database
+{
+    public static function connect(array $config): PDO
+    {
+        $dsn = sprintf(
+            'mysql:host=%s;port=%d;dbname=%s;charset=%s',
+            $config['host'],
+            $config['port'],
+            $config['name'],
+            $config['charset']
+        );
+
+        return new PDO($dsn, $config['user'], $config['pass'], [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ]);
+    }
+}
